a9cc2c481b0c04ac480ad06fe0efcd4c499fbeec
Mark Shroyer <code@markshroyer.com>
2012-11-23 14:36:33 -0500

    Add stubs.h and a check for UNIX98 PTYs

--- src/nulltty.c
+++ src/nulltty.c
@@ -1,8 +1,21 @@
+#include <stubs.h>
+
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Obtains a new pseudoterminal master file descriptor
+ *
+ * @return Master's file descriptor, or -1 on error
+ */
+int get_pty()
+{
+}
 
 int main(int argc, char* argv[])
 {
-  printf("Hello, world...\n");
+    printf("Hello, world...\n");
 
-  return 0;
+    return 0;
 }
