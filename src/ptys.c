da66eb83bdc30deafcdff95d863aec2edd4073a1
Mark Shroyer <code@markshroyer.com>
2012-12-16 18:08:43 -0500

    Preliminary support for building on OpenBSD
    Supports OpenBSD by allowing the use of openpty() in place of
    posix_openpt(), and select() in place of pselect().
    
    But we aren't done with the port yet: there is an autoconf script
    glitch preventing consistent addition of -lutil on OpenBSD, and the
    "make check" test program hangs after it runs.

--- src/ptys.c
+++ src/ptys.c
@@ -11,6 +11,9 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
+#ifdef HAVE_UTIL_H
+#include <util.h>
+#endif
 
 #include "ptys.h"
 
@@ -49,12 +52,16 @@ static inline ssize_t debug_read(int fd, void *buf, size_t count) {
     nreads++;
     return read(fd, buf, count);
 }
+#define read(...) debug_read(__VA_ARGS__)
 
 static inline ssize_t debug_write(int fd, const void *buf, size_t count) {
     nsyscalls++;
     nwrites++;
     return write(fd, buf, count);
 }
+#define write(...) debug_write(__VA_ARGS__)
+
+#ifdef HAVE_PSELECT
 
 static inline ssize_t debug_pselect(int nfds, fd_set *readfds, fd_set *writefds,
                                     fd_set *exceptfds, struct timespec *timeout,
@@ -64,10 +71,20 @@ static inline ssize_t debug_pselect(int nfds, fd_set *readfds, fd_set *writefds,
     nselects++;
     return pselect(nfds, readfds, writefds, exceptfds, timeout, sigmask);
 }
-
-#define read(...) debug_read(__VA_ARGS__)
-#define write(...) debug_write(__VA_ARGS__)
 #define pselect(...) debug_pselect(__VA_ARGS__)
+
+#else /* defined HAVE_PSELECT */
+
+static inline ssize_t debug_select(int nfds, fd_set *readfds, fd_set *writefds,
+                                   fd_set *exceptfds, struct timeval *timeout)
+{
+    nsyscalls++;
+    nselects++;
+    return select(nfds, readfds, writefds, exceptfds, timeout);
+}
+#define select(...) debug_select(__VA_ARGS__)
+
+#endif /* ! defined HAVE_PSELECT */
 
 #endif /* DEBUG */
 
@@ -75,44 +92,6 @@ static inline ssize_t debug_pselect(int nfds, fd_set *readfds, fd_set *writefds,
 /*** HELPER FUNCTIONS *********************************************************/
 
 #define MAX(a, b) ((a)>(b)) ? (a) : (b)
-
-static int platform_openpt()
-{
-    int fd;
-
-    /*
-     * We don't specify the O_NONBLOCK flag here, because it is a
-     * nonstandard flag to the posix_openpt() system call and results in an
-     * error on FreeBSD 9.0. And we do not set O_NONBLOCK with fcntl()
-     * after the fact because this results in an error, with errno = ENOTTY
-     * on OS X 10.8.
-     *
-     * So instead of opening our PTY masters in non-blocking mode, we rely
-     * on the behavior of the main select loop that we never attempt to
-     * read from a master unless we can do so without blocking.
-     */
-    fd = posix_openpt(O_RDWR | O_NOCTTY);
-    if ( fd < 0 )
-        goto error;
-
-    if ( grantpt(fd) < 0 )
-        goto error_opened;
-
-    if ( unlockpt(fd) < 0 )
-        goto error_opened;
-
-    return fd;
-
- error_opened:
-    close(fd);
- error:
-    return -1;
-}
-
-static int platform_closept(int fd)
-{
-    return close(fd);
-}
 
 /**
  * Open a single PTY nulltty endpoint
@@ -125,14 +104,33 @@ static int platform_closept(int fd)
  * @param link Name of symbolic link requested for this PTY slave
  * @return 0 on success, -1 with errno on error
  */
-static int openpty(struct nulltty_pty *pty, const char *link)
+static int endpoint_open(struct nulltty_pty *pty, const char *link)
 {
     int link_len;
     struct termios t = { 0 };
 
-    pty->fd = platform_openpt();
+#ifdef HAVE_POSIX_OPENPT
+
+    /*
+     * We don't specify the O_NONBLOCK flag here, because it is a
+     * nonstandard flag to the posix_openpt() system call and results in an
+     * error on FreeBSD 9.0. And we do not set O_NONBLOCK with fcntl()
+     * after the fact because this results in an error, with errno = ENOTTY
+     * on OS X 10.8.
+     *
+     * So instead of opening our PTY masters in non-blocking mode, we rely
+     * on the behavior of the main select loop that we never attempt to
+     * read from a master unless we can do so without blocking.
+     */
+    pty->fd = posix_openpt(O_RDWR | O_NOCTTY);
     if ( pty->fd < 0 )
         goto error_openpt;
+
+    if ( grantpt(fd) < 0 )
+        goto error_opened;
+
+    if ( unlockpt(fd) < 0 )
+        goto error_opened;
 
     /*
      * On Linux at least, when the last fd of the slave PTY is closed an
@@ -143,7 +141,17 @@ static int openpty(struct nulltty_pty *pty, const char *link)
      */
     pty->slave_fd = open(ptsname(pty->fd), O_RDWR | O_NOCTTY | O_NONBLOCK);
     if ( pty->slave_fd < 0 )
-        goto error_open_slave;
+        goto error_opened;
+
+#else /* defined POSIX_OPENPT */
+
+    /* The name parameter to openpty() needs to contain "at least sixteen
+     * characters" according to the man page in OpenBSD 5.2... */
+    char pty_slave_name[16];
+    if ( openpty(&pty->fd, &pty->slave_fd, pty_slave_name, NULL, NULL) < 0 )
+        goto error_openpt;
+
+#endif /* ! defined POSIX_OPENPT */
 
     /*
      * Put the slave pty fd into raw mode.
@@ -171,8 +179,13 @@ static int openpty(struct nulltty_pty *pty, const char *link)
         goto error_read_buf;
     pty->read_n = 0;
 
+#ifdef HAVE_PTSNAME
     if ( symlink(ptsname(pty->fd), link) < 0 )
         goto error_symlink;
+#else
+    if ( symlink(pty_slave_name, link) < 0 )
+        goto error_symlink;
+#endif
 
     return 0;
 
@@ -183,7 +196,9 @@ static int openpty(struct nulltty_pty *pty, const char *link)
  error_termios:
  error_link_name:
     close(pty->slave_fd);
- error_open_slave:
+#ifdef HAVE_POSIX_OPENPT
+ error_opened:
+#endif
     close(pty->fd);
  error_openpt:
     return -1;
@@ -197,14 +212,14 @@ static int openpty(struct nulltty_pty *pty, const char *link)
  * @param pty Pseudoterminal descriptor to close
  * @return 0 on success, -1 with errno on error
  */
-static int closepty(struct nulltty_pty *pty)
+static int endpoint_close(struct nulltty_pty *pty)
 {
     int result = 0;
 
     if ( close(pty->slave_fd) < 0 )
         result = -1;
 
-    if ( platform_closept(pty->fd) < 0 )
+    if ( close(pty->fd) < 0 )
         result = -1;
 
     if ( unlink(pty->link) < 0 )
@@ -306,16 +321,16 @@ nulltty_t nulltty_open(const char *link_a, const char *link_b)
     if ( nulltty == NULL )
         goto error_nulltty;
 
-    if ( openpty(&nulltty->a, link_a) < 0 )
+    if ( endpoint_open(&nulltty->a, link_a) < 0 )
         goto error_link_a;
 
-    if ( openpty(&nulltty->b, link_b) < 0 )
+    if ( endpoint_open(&nulltty->b, link_b) < 0 )
         goto error_link_b;
 
     return nulltty;
 
  error_link_b:
-    closepty(&nulltty->a);
+    endpoint_close(&nulltty->a);
  error_link_a:
     free(nulltty);
  error_nulltty:
@@ -326,8 +341,8 @@ int nulltty_close(nulltty_t nulltty)
 {
     int result = 0;
 
-    result += closepty(&nulltty->a);
-    result += closepty(&nulltty->b);
+    result += endpoint_close(&nulltty->a);
+    result += endpoint_close(&nulltty->b);
     free(nulltty);
 
     return result;
@@ -360,7 +375,20 @@ int nulltty_relay(nulltty_t nulltty, volatile sig_atomic_t *exit_flag)
         if ( *exit_flag != 0 )
             goto end_masked;
 
+#ifdef HAVE_PSELECT
+
         if ( pselect(nfds, &rfds, &wfds, NULL, NULL, &prev_set) < 0 ) {
+
+#else /* defined HAVE_PSELECT */
+
+        if ( sigprocmask(SIG_SETMASK, &prev_set, NULL) < 0 ) {
+            result = -1;
+            goto end;
+        }
+        if ( select(nfds, &rfds, &wfds, NULL, NULL) < 0 ) {
+
+#endif /* ! defined HAVE_PSELECT */
+
             switch ( errno ) {
             case EINTR:
                 sigprocmask(SIG_SETMASK, &prev_set, NULL);
