e6cbbb541b6887d10885fa858698a7f4f6084dec
Mark Shroyer <code@markshroyer.com>
2013-01-14 22:28:41 -0500

    Move config.h inclusion to top of stubs.h

--- stubs.h
+++ stubs.h
@@ -1,12 +1,12 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #if __APPLE__
 #define daemon yes_we_know_that_daemon_is_deprecated_in_os_x_10_5_thankyou
 #include <stdlib.h>
 #undef daemon
 extern int daemon(int, int);
-#endif
-
-#ifdef HAVE_CONFIG_H
-#include "config.h"
 #endif
 
 #include <string.h>
